13c6fce797f3e63576d1521d77106213429f1292
kaktus education <130348932+kaktuseducation@users.noreply.github.com>
2023-11-23 02:56:55 +0500

    Update teams.js

--- js/teams.js
+++ js/teams.js
@@ -1,1 +1,43 @@
+TEAMS = {
+  'Alim' :
+  `Name : Alim [Álim] \n
+  Birthday : 10.02.2019 \n
+  Fandom : \n
+  Debut cover : \n
+  Official color : purple, pink, white \n
+  Members : Sasha \n
+            Dosha \n
+            Vika \n
+            Dasha \n
+            Masha \n
+            Nastya \n
+            Senya \n
+            Ivi \n
+            Natasha \n
+            Sara \n
+            Cana \n
+            Taya \n
+            Alina \n
+            Yan \n
+            Nasya \n
+            Diana \n
+            Sonya \n
+  About group : \n
+  SNS : \n' `,
 
+  'Blueming' :
+  `Name : Blueming [Blúeming] \n
+  Birthday : 08.08.2022 \n
+  Fandom : \n
+  Debut cover : LE SSERAFIM - FEARLESS \n
+  Official color : blue \n
+  Members : Margo \n
+            Ariuna \n
+            Soonie \n
+            Barney \n
+            Dully \n
+            Finik \n
+            Hyunii \n
+            Pauline \n
+  About group : \n
+  SNS : \n' `,
