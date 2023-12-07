95642d05740543f725ca158fcdc3a43d766db135
kaktus education <130348932+kaktuseducation@users.noreply.github.com>
2023-12-08 00:17:58 +0500

    Update teams.js

--- js/teams.js
+++ js/teams.js
@@ -10,5 +10,41 @@ TEAMS = {
     'About group' : ' ',
     'SNS' : ['instagram', 'telegram', 'youtube'],
   },
-  
+
+  'Blueming' : {
+    'Name' : 'Blueming [Blúeming]',
+    'Birthday' : '08.08.2022',
+    'Fandom' : ' ',
+    'Debut cover' : 'LE SSERAFIM - FEARLESS',
+    'Official color' : ['blue'],
+    'Members' : ['Margo', 'Ariuna', 'Soonie', 'Barney', 'Dully', 'Finik', 'Hyunii', 'Pauline'], 
+    'About group' : ' ',
+    'SNS' : ['instagram', 'telegram', 'youtube'],
+},
+
+  'Keysd' : {
+    'Name' : 'Key-sd [Ki-sd]', 
+    'Birthday' : '08.11.2021', 
+    'Fandom' : ' ',
+    'Debut cover' : ' ',
+    'Official color' : ['blue'], 
+    'Members' : ['Cat',  'Kiki', 'Senya', 'Dilya', 'Misa', 'Sasha', 'Docha', 'Yulya', 'Ari', 'Lina' 'Hanie'],
+    'About group' : ' ',
+    'SNS' : ['instagram', 'telegram', 'youtube'],
+  },
+
+  'Obsession' : 
+    'Name : Obsession [əbˈsɛ́ʃᵊn]
+    Birthday : 29.02.2023
+    Fandom :
+    Debut cover : BVNDT - VENOM
+    Official color : red
+    Members : Amina
+            Helli
+            Q
+            Eunji
+            Nika
+            Nastya
+    About group :
+    'SNS' : ['instagram', 'telegram', 'youtube'],
 }
