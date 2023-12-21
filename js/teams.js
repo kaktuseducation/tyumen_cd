f011280cace6b5e3f54365a4b68e741b33da9e5a
kaktus education <130348932+kaktuseducation@users.noreply.github.com>
2023-12-22 03:01:28 +0500

    Update teams.js

--- js/teams.js
+++ js/teams.js
@@ -53,5 +53,93 @@ TEAMS = {
     'Members' : ['Torry', 'Myun', 'Khosu', 'Busya', 'Misako', 'Seonha', 'Miku', 'Luna'],
     'About group' : ' ',
     'SNS' : ['instagram', 'telegram', 'youtube'],
+  },
+
+  'Stellare' :
+  `Name : Stellare [Stellare] \n
+  Birthday : 10.08.2023 \n
+  Fandom : Stellnes\n
+  Debut cover : \n
+  Official color : \n
+  Members : Ayun \n
+            Roa \n
+            Minty \n
+            Haotin \n
+  About group : \n
+  SNS : \n `,
+
+  'The girlz' :
+  `Name : The girlz [ðə gi̇́rlz] \n
+  Birthday : 08.09.2021 \n
+  Fandom : \n
+  Debut cover : \n
+  Official color : blue \n
+  Members : RM \n
+            Leco \n
+            Minci \n
+            Ari \n
+            Icy \n
+            Renata \n
+            Darcy \n      
+  About group : \n
+  SNS : \n `,
+
+  'W2H' :
+  `Name : W2H [wiːtu:éɪʧ] \n
+  Birthday : 15.01.2023 \n
+  Fandom : Devils [dɛ́vᵊlz] \n
+  Debut cover : STRAY KIDS - THUNDEROUS \n
+  Official color : black, red \n
+  Members : Tate \n
+            Sato \n
+            Lexus \n
+            Hani \n
+            Mins \n
+            ReiLi \n
+            Lin \n
+            Mellet \n
+  About group : \n
+  SNS : \n `,
+
+  'Werise' :
+  `Name : Werise [Weri̇́se] \n
+  Birthday : 07.10.2021 \n
+  Fandom : \n
+  Debut cover : LE SSERAFIM - ANTIFRIAGLE \n
+  Official color : blue, white \n
+  Members : Vishes \n
+            LU \n
+            Nuta \n
+            Aizhan \n
+            Lana \n
+            Fisa \m
+  About group : \n
+  SNS : \n `,
+
+    'Wings of passion' :
+  `Name : Wings of passion [wɪŋz ɒv ˈpæʃᵊn] \n
+  Birthday : 29.09.2022 \n
+  Fandom : \n
+  Debut cover : TEN & WINWIN - LOVELY \n
+  Official color : black, gray, white \n
+  Members : Alice Lehodey \n
+  About group : \n
+  SNS : \n `,
+
+  'Wisteria' :
+  `Name : Wisteria [wɪˈstɪə́riə] \n
+  Birthday : 07.03.2023 \n
+  Fandom : \n
+  Debut cover : NEWJEANS - OMG \n
+  Official color : light purple \n
+  Members : Mint \n
+            Raf \n
+            Mona \n
+            Evy \n
+            Wqouip \n
+            Alisqex \n
+            Vita \n
+  About group : \n
+  SNS : \n `,
 
 }
